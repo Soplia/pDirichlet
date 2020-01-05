835af9a5af3817a2b5f371bf37dc10ca91b6aa97
jiaRuiXie <jiarui.xie@isima.fr>
2020-01-05 17:14:48 +0100

    jrx-alpha-debugAnalise200105E

--- analyse.py
+++ analyse.py
@@ -10,41 +10,57 @@ import matplotlib.pyplot as plt
 
 # Read Data
 npzfile = np.load('../data/outputs_test.npz')
+# (8400, 10)
 outputs = npzfile['arr_0']
+# (8400)
 targets = npzfile['arr_1']
 
 # Apply Relu
 outputs = outputs * (outputs > 0) 
 
 # Comp prob
-Se = np.sum(outputs,axis=1)
+# (8400)
+Se = np.sum(outputs, axis=1)
+# (10, 8400), column is same
 Se = np.expand_dims(Se, axis=0)
-Se = np.repeat(Se, outputs.shape[1],axis = 0)
+# (8400, 10)
+Se = np.repeat(Se, outputs.shape[1], axis = 0)
 Se = np.transpose(Se)
+# (8400, 10)
 Pr = outputs / Se
-
-
-# evidence = relu_evidence(outputs)
-# alpha = evidence + 1
-# u = numOfClass / torch.sum(alpha, dim = 1, keepdims = True)
-# pro = alpha / torch.sum (alpha, dim = 1, keepdims = True)
 
 # Comp Belief
 alpha = outputs + 1
 Sa = np.sum(alpha,axis=1)
 Sa = np.expand_dims(Sa, axis=0)
 Sa = np.repeat(Sa, alpha.shape[1],axis = 0)
+# (8400, 10)
 Sa = np.transpose(Sa)
 Be = outputs / Sa
 
 # Comp uncertaintly
 K = outputs.shape[1]
+#(8400)
 U = K / Sa[:,0]
+
+######################################JrX-Add-S#############
+# print ('Calculating...')
+# numClass = 10
+# # (8400, 10)
+# Be = outputs
+# evidence = Be * (Be > 0)
+# alpha = torch.from_numpy(evidence + 1)
+# # (8400, 10)
+# Pr = (alpha / torch.sum(alpha, dim = 1, keepdims = True)).numpy()
+# # (8400, 1)
+# U = (numClass / torch.sum(alpha, dim = 1, keepdims = True)).numpy()
+######################################JrX-Add-E#############
 
 # Sort belief and prob
 classes = []
 beliefs = []
 probas = []
+# Be (8400,10)
 for i in range(Be.shape[0]):
       oneBe = Be[i,:]
       onePr = Pr[i,:]
@@ -56,6 +72,7 @@ for i in range(Be.shape[0]):
       probas.append(Prs)
 
 classes = np.concatenate(classes, axis=0)
+# conver list to np.array (8400, 10)
 classes = classes.reshape(outputs.shape)
 beliefs = np.concatenate(beliefs, axis=0)
 beliefs = beliefs.reshape(outputs.shape)
@@ -73,6 +90,7 @@ probasCS = np.cumsum(probas,axis=1)
 targetsTable = (targets + 1)
 targetsTable = np.expand_dims(targetsTable , axis=0)
 targetsTable = np.repeat(targetsTable, outputs.shape[1],axis = 0)
+# (8400, 10)
 targetsTable = np.transpose(targetsTable)
 classes = classes + 1
 nb = 100 # Nb of points
@@ -105,9 +123,8 @@ for th in  np.linspace(0, 1, num=nb):
       # print(f'accPr = {accPr}, mean classes = {meanClPr}')
 
 plt.plot(SclBe,SaccBe,label='Using Belief threshold')
-plt.plot(SclPr,SaccPr,label='Using SoftMax threshold')
+plt.plot(SclPr,SaccPr,label='Using Prob threshold')
 plt.legend()
 plt.title('Accuracy vs Mean nb of candidates')
 plt.show()
 #pdb.set_trace()
-
